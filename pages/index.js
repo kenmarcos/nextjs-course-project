3210bfadab2f37a8de2563262f34c8fe6eed5ac2
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-02-10 16:11:52 -0300

    feat: add SSG on the home page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import { EventList } from "../components/events/event-list";
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 
-const HomePage = () => {
-  const featuredEvents = getFeaturedEvents();
-
+const HomePage = ({ featuredEvents }) => {
   return (
     <>
       <h1 className="center title">Featured Events</h1>
       <EventList events={featuredEvents} />
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      featuredEvents,
+    },
+  };
 };
 
 export default HomePage;
